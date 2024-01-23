0f8a4e714f89016f8ee798597ecb845b24e8d7fd
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-23 13:33:14 +0100

    create country list item

---
+++ UserControls/CountryListItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDashboard2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CountryListItem.xaml
+    /// </summary>
+    public partial class CountryListItem : UserControl
+    {
+        public CountryListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
