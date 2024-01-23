31ef5d3e0eaee20be5bf91ab63c2bfa38b06e63b
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-23 13:41:27 +0100

    implement code behind for country list items

--- UserControls/CountryListItem.xaml.cs
+++ UserControls/CountryListItem.xaml.cs
@@ -20,6 +20,54 @@ namespace WPFDashboard2.UserControls
     /// </summary>
     public partial class CountryListItem : UserControl
     {
+        public static readonly DependencyProperty CountryNameProperty = DependencyProperty.Register(
+            "CountryName",
+            typeof(string),
+            typeof(CountryListItem)
+        );
+
+        public static readonly DependencyProperty PriceProperty = DependencyProperty.Register(
+            "Price",
+            typeof(string),
+            typeof(CountryListItem)
+        );
+
+        public static readonly DependencyProperty IsLevelUpProperty = DependencyProperty.Register(
+            "IsLevelUp",
+            typeof(bool),
+            typeof(CountryListItem)
+        );
+
+        public static readonly DependencyProperty FlagProperty = DependencyProperty.Register(
+            "Flag",
+            typeof(ImageSource),
+            typeof(CountryListItem)
+        );
+
+        public string CountryName
+        {
+            get => (string)GetValue(CountryNameProperty);
+            set => SetValue(CountryNameProperty, value);
+        }
+
+        public string Price
+        {
+            get => (string)GetValue(PriceProperty);
+            set => SetValue(PriceProperty, value);
+        }
+
+        public bool IsLevelUp
+        {
+            get => (bool)GetValue(IsLevelUpProperty);
+            set => SetValue(IsLevelUpProperty, value);
+        }
+
+        public ImageSource Flag
+        {
+            get => (ImageSource)GetValue(FlagProperty);
+            set => SetValue(FlagProperty, value);
+        }
+
         public CountryListItem()
         {
             InitializeComponent();
